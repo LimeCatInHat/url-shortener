187f94cccf13192d898de3e47502051c2b140bf3
cat-in-magic-hat <nniki.feliz@gmail.com>
2024-05-18 13:52:16 +0300

    add response

--- internal/logger/request-logger.go
+++ internal/logger/request-logger.go
@@ -1,24 +1,70 @@
 package logger
 
 import (
+	"net/http"
 	"time"
 
 	"go.uber.org/zap"
 )
 
-type RequestLogger struct {
-	baseLogger zap.SugaredLogger
+type (
+	RequestLogger struct {
+		logHandler *logHandler
+		baseLogger zap.SugaredLogger
+	}
+
+	logHandler struct {
+		http.ResponseWriter
+		responseData *responseData
+		request      *http.Request
+	}
+
+	responseData struct {
+		status int
+		size   int
+	}
+)
+
+func (requestLogger *RequestLogger) HandleRequest(request *http.Request, w http.ResponseWriter) {
+	h := &logHandler{
+		ResponseWriter: w,
+		request:        request,
+		responseData: &responseData{
+			status: 0,
+			size:   0,
+		},
+	}
+	requestLogger.logHandler = h
 }
 
-func (requestLogger RequestLogger) LogRequestInfo(uri string, method string, duration time.Duration) {
+func (requestLogger *RequestLogger) GetLoggingResponseWriter() http.ResponseWriter {
+	return requestLogger.logHandler
+}
+
+func (requestLogger *RequestLogger) LogRequestInfo(duration time.Duration) {
+	request := requestLogger.logHandler.request
+
 	requestLogger.baseLogger.Infoln(
-		"uri", uri,
-		"method", method,
+		"uri", request.RequestURI,
+		"method", request.Method,
+		"status", requestLogger.logHandler.responseData.status,
 		"duration", duration,
+		"size", requestLogger.logHandler.responseData.size,
 	)
+}
+
+func (r *logHandler) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *logHandler) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
 }
 
 func CreateRequestLogger() (RequestLogger, error) {
 	baseLogger, err := createLogger()
-	return RequestLogger{baseLogger}, err
+	return RequestLogger{baseLogger: baseLogger}, err
 }
--- internal/middlewares/log-middleware.go
+++ internal/middlewares/log-middleware.go
@@ -3,23 +3,20 @@ package middlewares
 import (
 	"net/http"
 	"time"
+
+	"github.com/LimeCatInHat/url-shortener/internal/logger"
 )
 
-type RequestLogger interface {
-	LogRequestInfo(uri string, method string, duration time.Duration)
-}
-
-func WithLogging(h http.Handler, log RequestLogger) http.HandlerFunc {
+func WithLogging(h http.Handler, log logger.RequestLogger) http.HandlerFunc {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		log.HandleRequest(r, w)
 
-		uri := r.RequestURI
-		method := r.Method
-		h.ServeHTTP(w, r)
+		h.ServeHTTP(log.GetLoggingResponseWriter(), r)
 
 		duration := time.Since(start)
 
-		log.LogRequestInfo(uri, method, duration)
+		log.LogRequestInfo(duration)
 	}
 	return http.HandlerFunc(logFn)
 }
